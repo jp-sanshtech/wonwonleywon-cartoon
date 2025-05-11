e789008a10e3025ae1cc31f7154b911fa680215d
jp-sanshtech <jp@sanshtech.ca>
2025-05-10 22:47:38 -0600

    sadfas

--- src/components/main/Head.jsx
+++ src/components/main/Head.jsx
@@ -9,7 +9,7 @@ const Head = () => {
       <Link to="/">
         <img 
           src={logo} 
-          style={{ width: '157px' , height: 'auto' , marginLeft:"6px" , marginTop : "3px"}} // Adjust the size as necessary
+          style={{ width: '157px' , height: 'auto' , marginLeft:"2px" }} // Adjust the size as necessary
           alt="Logo" 
           className={classes.logo}
         />
